75b86a11097175c2bbbb33813730f58f5fbb9419
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-23 17:19:03 +0530

    Updated the source and test codes

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,14 +1,25 @@
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using AggregateGDPPopulation;
+using System.IO;
+using System.Threading.Tasks;
 using Xunit;
+
 
 namespace AggregateGDPPopulation.Tests
 {
-    public class UnitTest1
+    public class AggregateGDPPopulationTesting
     {
         [Fact]
-        public void Test1()
+        public  async void Test1()
         {
-
+            Task process1 = Async.CalculateGDPPopulation();
+            await process1;
+            JObject Actual = JObject.Parse(File.ReadAllText("../../../../AggregateGDPPopulation/data/outputfilenew.json"));
+            JObject Expected = JObject.Parse(File.ReadAllText("../../../expected-output.json"));
+            Assert.Equal(Expected, Actual);
+            Assert.Equal(Expected, Actual);
         }
     }
 }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,68 @@
 ﻿using System;
 
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
 namespace AggregateGDPPopulation
 {
-    public class Class1
+    public static class Async
     {
+            public static async Task CalculateGDPPopulation()
+            {
+                string dataFilePath = @"D:\C# assignments\aggregate-gdp-population-csharp-problem-Ritam-majumdar\AggregateGDPPopulation\data\datafile.csv";
+                string mapperFilePath = @"D:\C# assignments\aggregate-gdp-population-csharp-problem-Ritam-majumdar\AggregateGDPPopulation\data\country-continent.json";
+                string writeFilePath = @"D:\C# assignments\aggregate-gdp-population-csharp-problem-Ritam-majumdar\AggregateGDPPopulation\data\outputfilenew.json";
+                JObject CountryVsContinent = JObject.Parse(await File.ReadAllTextAsync(mapperFilePath));
+                List<string> rows = new List<string>();
+                string line;
+                string line1;
+                //string[] data = File.ReadAllLines(dataFilePath);
+                string[] data = await File.ReadAllLinesAsync(dataFilePath);
+                foreach (string str in data)
+                {
+                    line = str.Replace("[\"]+", "");
+                    rows.Add(line);
+                }
+                string[] headers;
+                headers = rows[0].Split(',');
+                int CountryIndex = Array.IndexOf(headers, "\"Country Name\"");
+                int PopulationIndex = Array.IndexOf(headers, "\"Population (Millions) - 2012\"");
+                int GDPIndex = Array.IndexOf(headers, "\"GDP Billions (US Dollar) - 2012\"");
+                rows.RemoveAt(0);
+                Dictionary<string, Dictionary<string, double>> resultset = new Dictionary<string, Dictionary<string, double>>();
+                string[] rowdata;
+                foreach (string row in rows)
+                {
+                    line1 = row.Replace("\"", "");
+                    rowdata = line1.Split(',');
+                    try
+                    {
+                        string continent = CountryVsContinent[rowdata[CountryIndex]].ToString();
+
+                        if (resultset.ContainsKey(continent))
+                        {
+                            double GDPToAdd = Convert.ToDouble(rowdata[GDPIndex]);
+                            double PopulationToAdd = Convert.ToDouble(rowdata[PopulationIndex]);
+                            resultset[continent]["GDP_2012"] = resultset[continent]["GDP_2012"] + GDPToAdd;
+                            resultset[continent]["POPULATION_2012"] = resultset[continent]["POPULATION_2012"] + PopulationToAdd;
+                        }
+                        else
+                        {
+                            Dictionary<string, double> GDPVsPopulation = new Dictionary<string, double>();
+                            GDPVsPopulation.Add("GDP_2012", Convert.ToDouble(rowdata[GDPIndex]));
+                            GDPVsPopulation.Add("POPULATION_2012", Convert.ToDouble(rowdata[PopulationIndex]));
+                            resultset.Add(continent, GDPVsPopulation);
+                        }
+                    }
+                    catch { };
+
+                }
+                string Json = JsonConvert.SerializeObject(resultset, Formatting.Indented);
+                 await File.WriteAllTextAsync(writeFilePath, Json);
+            }
     }
 }
